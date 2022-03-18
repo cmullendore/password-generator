d4018148cca2ab34491e63d20a0d1994da9f957e
Chris Mullendore <chris@vindage.com>
2022-03-17 18:13:27 -0700

    Refactor into better OOP

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,8 +6,6 @@ const absSpecial = " !\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~";
 const absMinLength = 8;
 const absMaxLength = 128;
 
-var advancedForm = document.getElementById("advancedForm");
-advancedForm.onsubmit = function(e) { e.preventDefault(); }
 
 var password = {
   length: 8,
@@ -15,8 +13,8 @@ var password = {
   numbers: "0123456789",
   special: " !\"#$%&'()*+,-./:;&lt;=>?@[\\]^_`{|}~",
 
-  generate: function(allowedChars) {
-    var charsArray = allowedChars.split("");
+  generate: function() {
+    var charsArray = (this.letters + this.numbers + this.special).split("");
     var generatedPassword = [];
     for (i = 0; i < this.length; i++) {
       var nextChar = charsArray[Math.floor(Math.random() * charsArray.length)]
@@ -42,18 +40,19 @@ var resetBtn = document.querySelector("#reset");
 // Write password to the #password input
 function writePassword() {
 
-  length = document.getElementById("allowed-length").value;
-  letters = document.getElementById("allowed-letters").value;
-  numbers = document.getElementById("allowed-numbers").value;
-  special = document.getElementById("allowed-special").value;
-
-  var allowedChars = letters + numbers + special;
+  password.length = document.getElementById("allowed-length").value;
+  password.letters = document.getElementById("allowed-letters").value;
+  password.numbers = document.getElementById("allowed-numbers").value;
+  password.special = document.getElementById("allowed-special").value;
 
   var passwordText = document.querySelector("#password");
 
-  passwordText.value = password.generate(allowedChars);
+  passwordText.value = password.generate();
 
 }
+
+var advancedForm = document.getElementById("advancedForm");
+advancedForm.onsubmit = function(e) { e.preventDefault(); }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
